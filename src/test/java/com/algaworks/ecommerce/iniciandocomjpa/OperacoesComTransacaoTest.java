3ef097d1f47ec4bb9c8c8a16043b795bc01c556f
robertocajueiro <beto.cajueiro@gmail.com>
2022-12-24 13:48:34 -0300

    2.14. Entendendo a diferença entre os métodos persist e merge

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -10,6 +10,62 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    public void mostrarDiferencaPersistMerge(){
+        Produto produtoPersist = new Produto();
+
+        produtoPersist.setId(5);
+        produtoPersist.setNome("SmartPhone One Plus");
+        produtoPersist.setDescricao("O processador mais rápido.");
+        produtoPersist.setPreco(new BigDecimal((2000)));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produtoPersist);
+        produtoPersist.setNome("SmartPhone Two Plus");
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacaoPersist = entityManager.find(Produto.class, produtoPersist.getId());
+        Assert.assertNotNull(produtoVerificacaoPersist);
+
+        Produto produtoMerge = new Produto();
+
+        produtoMerge.setId(6);
+        produtoMerge.setNome("Notebook Dell");
+        produtoMerge.setDescricao("O melhor da categoria.");
+        produtoMerge.setPreco(new BigDecimal((2000)));
+
+        entityManager.getTransaction().begin();
+        produtoMerge =  entityManager.merge(produtoMerge);
+        produtoMerge.setNome("Notebook Dell 2");
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacaoMerge = entityManager.find(Produto.class, produtoMerge.getId());
+        Assert.assertNotNull(produtoVerificacaoMerge);
+    }
+
+    @Test
+    public void inserirOObjetoComMerge(){
+        Produto produto = new Produto();
+
+        produto.setId(4);
+        produto.setNome("Microfone Rode Videmic");
+        produto.setDescricao("A melhor qualidade de som.");
+        produto.setPreco(new BigDecimal((1000)));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+    }
+
+    @Test
     public void atualizarObjetoGerenciado(){
         Produto produto = entityManager.find(Produto.class, 1);
 
