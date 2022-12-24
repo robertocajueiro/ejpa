71c88aed56856ab88dffe3263d302efba0bf1b1a
robertocajueiro <beto.cajueiro@gmail.com>
2022-12-24 16:18:57 -0300

    3.2. Mapeando as entidades e customizando os nomes das tabelas e colunas

--- src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -5,14 +5,17 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "cliente")
 public class Cliente {
 
     @EqualsAndHashCode.Include
--- src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -4,23 +4,28 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "nota_fiscal")
 public class NotaFiscal {
 
     @EqualsAndHashCode.Include
     @Id
     private Integer id;
 
+    @Column(name = "pedido_id")
     private Integer pedidoId;
 
     private String xml;
 
+    @Column(name = "data_emissao")
     private Date dataEmissao;
 }
