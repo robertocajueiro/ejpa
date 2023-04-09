19aae6a38d73bd87b01985f5fa522c3c6bb1659f
robertocajueiro <beto.cajueiro@gmail.com>
2023-04-09 18:24:20 -0300

    4.8. Mapeando relacionamentos muitos-para-muitos com @ManyToMany e @JoinTable

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
@@ -24,5 +25,10 @@ public class Produto {
     private String descricao;
 
     private BigDecimal preco;
+
+    @ManyToMany
+    @JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"),
+    inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias;
 
 }
