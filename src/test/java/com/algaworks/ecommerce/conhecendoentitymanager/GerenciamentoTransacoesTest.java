818fadd2315efd9772caface845897d9bf046067
robertocajueiro <beto.cajueiro@gmail.com>
2023-04-10 14:41:53 -0300

    5.3. Gerenciamento de transações

---
+++ src/test/java/com/algaworks/ecommerce/conhecendoentitymanager/GerenciamentoTransacoesTest.java
@@ -0,0 +1,31 @@
+package com.algaworks.ecommerce.conhecendoentitymanager;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.StatusPedido;
+import org.junit.Test;
+
+public class GerenciamentoTransacoesTest extends EntityManagerTest {
+
+    @Test(expected = Exception.class)
+    public void abrirFecharCancelarTransacao() {
+        try {
+            entityManager.getTransaction().begin();
+            metodoNegocio();
+            entityManager.getTransaction().commit();
+
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    private void metodoNegocio(){
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+        pedido.setStatus(StatusPedido.PAGO);
+
+        if (pedido.getPagamento() == null) {
+            throw new RuntimeException("Pedido ainda não foi pago.");
+        }
+    }
+}
