a5af89b080786376b281c6ce622aab08370bbcce
robertocajueiro <beto.cajueiro@gmail.com>
2023-06-04 10:25:30 -0300

    6.16. Entendendo a diferença entre estender uma entidade abstrata e usar a anotação @MappedSuperclass

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancado/HerancaTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/HerancaTest.java
@@ -1,11 +1,12 @@
 package com.algaworks.ecommerce.mapeamentoavancado;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class HerancaTest extends EntityManagerTest {
 
@@ -22,5 +23,33 @@ public class HerancaTest extends EntityManagerTest {
 
         Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
         Assert.assertNotNull(clienteVerificacao.getId());
+    }
+
+    @Test
+    public void buscarPagamentos(){
+        List<Pagamento> pagamentos = entityManager
+                .createQuery("select p from Pagamento p")
+                .getResultList();
+
+        Assert.assertFalse(pagamentos.isEmpty());
+    }
+
+    @Test
+    public void incluirPagamentoPedido() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setPedido(pedido);
+        pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
+        pagamentoCartao.setNumeroCartao("123");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pagamentoCartao);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerficacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerficacao.getPagamento());
     }
 }
