458f91efe57ab2e9a6bf5619321a3bc0d2066b77
robertocajueiro <beto.cajueiro@gmail.com>
2023-06-18 19:03:42 -0300

    7.9. Exercício: usando @JoinColumn

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -36,8 +36,11 @@ public class Produto extends EntidadeBaseInteger {
     private byte[] foto;
 
     @ManyToMany
-    @JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"),
-    inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    @JoinTable(name = "produto_categoria",
+            joinColumns = @JoinColumn(name = "produto_id", nullable = false,
+                foreignKey = @ForeignKey(name = "fk_produto_categoria_produto")),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id", nullable = false,
+                foreignKey = @ForeignKey(name = "fk_produto_categoria_categoria")))
     private List<Categoria> categorias;
 
     @OneToOne(mappedBy = "produto")
@@ -45,13 +48,15 @@ public class Produto extends EntidadeBaseInteger {
 
     @ElementCollection
     @CollectionTable(name = "produto_tag",
-            joinColumns = @JoinColumn(name = "produto_id"))
+            joinColumns = @JoinColumn(name = "produto_id", nullable = false,
+                foreignKey = @ForeignKey(name = "fk_produto_tag_produto")))
     @Column(name = "tag", length = 50, nullable = false)
     private List<String> tags;
 
     @ElementCollection
     @CollectionTable(name = "produto_atributo",
-            joinColumns = @JoinColumn(name = "produto_id"))
+            joinColumns = @JoinColumn(name = "produto_id", nullable = false,
+                foreignKey = @ForeignKey(name = "fk_produto_atributo_produto")))
     private List<Atributo> atributos;
 
 }
